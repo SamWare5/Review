8e0b4c7ef98f8c257748375296f4ec56873ca5e1
SamWare5 <Sahara5771>
2018-11-14 16:37:50 -0500

    Week3 Reviews

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,51 +1,16 @@
-print("Hello Python world!")
+for num in range(1, 101):
 
-message = "Hello Python world!"
-print(message)
+    print("")
 
-message = "Hello Python world!"
-print(message)
-message = "Hello Python Crash Course world!"
-print(message)
+    print(str(num) + ": ", end="")
 
+    if num % 3 == 0:
 
-print('I told my friend, "Python is my favorite language!"')
-print("The language 'Python' is named after Monty Python, not the snake.")
-print("One of Python's strengths is its diverse and supportive community.")
+        print("Fizz", end="")
 
+    if num % 5 == 0:
 
-name = "ada lovelace"
-print(name.title())
-print(name.upper())
-print(name.lower())
-
-first_name = "ada"
-last_name = "lovelace"
-full_name = first_name + " " + last_name
-print(full_name)
-
-first_name = "ada"
-last_name = "lovelace"
-full_name = first_name + " " + last_name
-print("Hello, " + full_name.title() + "!")
-
-first_name = "ada"
-last_name = "lovelace"
-full_name = first_name + " " + last_name
-message = "Hello, " + full_name.title() + "!"
-print(message)
-
-print("Python")
-print("\tPython")
-print("Languages:\nPython\nC\nJavaScript")
-print("Languages:\n\tPython\n\tC\n\tJavaScript")
-
-favorite_language = ' python and Java'
-#favorite_language = favorite_language.rstrip()
-#favorite_language = favorite_language.lstrip()
-favorite_language = favorite_language.strip()
-print(favorite_language)
-
+        print("Buzz", end="")
 
 
 
