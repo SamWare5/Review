af5bad58ce770ce96a2ba917b027ac88debb9fdf
SamWare5 <Sahara5771>
2018-11-14 16:58:01 -0500

    Week3 Reviews

---
+++ FizzBuzz2.py
@@ -0,0 +1,10 @@
+for n in range (25):
+    if n % 3 == 0 and n % 5 == 0:
+        print ("FizzBuzz")
+        if n % 3 == 0:
+            print("Fizz")
+            if n % 5 == 0:
+                print("Buzz")
+    print(n)
+
+
