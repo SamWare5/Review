008d29c2afd7e6113cd9a43324cd59532c392241
SamWare5 <Sahara5771>
2018-11-15 12:21:31 -0500

    Week3 Reviews

---
+++ FinalReviewPart1.py
@@ -0,0 +1,34 @@
+
+age = 20
+age2 = 20
+resultAge = age + age2
+print(resultAge)
+
+name = "Champlain"
+print("I am taking this course at " + name)
+
+sale1 = "1.40"
+sale2 = "2.30"
+
+print("The total price is $ " + str((float(sale1)) + (float(sale2))))
+
+
+
+for pos1 in range (2):
+
+    for pos2 in range(2):
+
+        for pos3 in range(2):
+
+            print(str(pos1)+str(pos2)+str(pos3))
+
+message = ""
+
+while message != 'quit':
+    message = input("Enter command :")
+    print(message)
+
+while True:
+    input(print(message))
+    message != 'quit':
+
--- FizzBuzz2.py
+++ FizzBuzz2.py
@@ -1,6 +1,6 @@
 for n in range (25):
     if n % 3 == 0 and n % 5 == 0:
-        print ("FizzBuzz")
+        print("FizzBuzz")
         if n % 3 == 0:
             print("Fizz")
             if n % 5 == 0:
