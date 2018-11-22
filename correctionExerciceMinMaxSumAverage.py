a718cec32458be740461f33d696517a54a03d235
SamWare5 <Sahara5771>
2018-11-22 10:21:17 -0500

    Week3 Reviews

---
+++ correctionExerciceMinMaxSumAverage.py
@@ -0,0 +1,30 @@
+import random
+
+my_list = []
+for x in range(10):
+    my_list.append(random.randint(0, 100))
+max_of_list = my_list[0]
+min_of_list = my_list[0]
+sum_of_list = 0
+length_of_list = 0
+
+print(my_list)
+
+for x in my_list:
+    length_of_list +=1
+    sum_of_list += x
+
+    if x > max_of_list:
+        max_of_list = x
+    if x < min_of_list:
+        min_of_list = x
+print("Sum = " + str(sum_of_list))
+print("Size = " + str(length_of_list))
+print("Greatest " + str(max_of_list))
+print("Lowest " + str(min_of_list))
+
+average_of_list = sum_of_list / length_of_list
+print("Average " + str(average_of_list))
+
+
+
