da7f4cad8f93cc7a6863ea13a264fa5f743b02be
SamWare5 <Sahara5771>
2018-11-29 12:41:39 -0500

    Week3 Reviews

---
+++ fizzbuzzFunctionTest.py
@@ -0,0 +1,15 @@
+def fizzBuzz():
+    def num_case(num, test_num):
+        if num%test_num==0:
+            return True
+    for num in range (0, 21):
+        print((str(num) + ":")
+        if (num_case(num, 3) and (num_case(nuprint("FizzBuzz")
+        elif (num_case(num, 3)):
+            print("Fizz")
+        elif (num_case(num, 5)):
+            print("Buzz")
+        else:
+            print()
+
+fizzBuzz()
