58a0d36cdc96f19fcfa42a85efd7a13822c149c7
SamWare5 <Sahara5771>
2019-01-29 12:40:58 -0500

    Week3 Reviews

---
+++ review_classes.py
@@ -0,0 +1,20 @@
+class Person:
+   # def __init__(self, name, age):
+     #   self.name = name
+      #  self.age = age
+
+
+
+#p1 = Person("John", 36)
+#print(p1.name)
+#print(p1.age)
+
+    name = " "
+    __age = 0
+
+def setAge(self, a):
+
+    p = Person()
+    p.setAge(22)
+print(p.Per
+
