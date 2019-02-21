c4bd26411f78be007b21d8bb101cace094f1fd0b
SamWare5 <Sahara5771>
2019-02-21 11:54:41 -0500

    Week3 Reviews

---
+++ unitest.py
@@ -0,0 +1,15 @@
+import unittest
+
+class Tests(unittest.case):
+
+    def test(self):
+        self.assertEqual(8, 8)
+    def test2(self):
+        self.asserEqual(8, 8)
+        
+
+    def add(num1, num2):
+
+        #this function adds numbers in spectacular way
+
+        return num1
